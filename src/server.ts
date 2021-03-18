64e44e4cab943f53131976119eff56fbcbc2f15b
Quentin Kermaidic <quentin.kermaidic@la-forge.co>
2021-03-18 11:14:23 +0100

    empty commit

--- src/server.ts
+++ src/server.ts
@@ -14,13 +14,13 @@ const store = { message: 'init' }
 
 router.get('/', (req, res) => res.send({ message: store.message}))
 router.post('/', (req, res) => {
-  console.log(req.body)
   if (!req.body || !req.body.message || typeof req.body.message !== 'string') throw new Error('Invalid body')
 
   store.message = req.body.message
 
   res.send({ success: true })
 })
+
 app.use('/api', router)
 
 export default {
